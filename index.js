a37ad46cf8993e7beacb32de76872537f37f28e1
Gary Johnson <garycjohnso@gmail.com>
2015-09-17 23:44:04 -0400

    p

--- index.js
+++ index.js
@@ -23,13 +23,12 @@ function pushUpdate(new_data) {
 // setTimeout(function() {addData({ "date":(new Date()).getTime()+3000, "data":(Math.floor(Math.random() * 6) + 1) });}, 1000);
 // setTimeout(function() {addData({ "date":(new Date()).getTime()+4000, "data":(Math.floor(Math.random() * 6) + 1) });}, 1000);
 // setInterval(function() {pushUpdate({ "date":(new Date()).getTime(), "data":(Math.floor(Math.random() * 6) + 1) });}, 1000);
+setInterval(function() {pushUpdate({ "date":(new Date()).getTime(), "data":(Math.floor(Math.random() * 6) + 1) });}, 1000);
 
 
 io.on('connection', function(socket){
   console.log('a user connected');
   // io.emit('completeData', data);
-  setInterval(function() {pushUpdate({ "date":(new Date()).getTime(), "data":(Math.floor(Math.random() * 6) + 1) });}, 2000);
-
 });
 
 server.listen(3000, function () {
